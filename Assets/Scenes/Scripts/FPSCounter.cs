0ba7f64febf14bb26be72224932a336ce3cc35cd
Kevin Turcumani <kevinjaja98@gmail.com>
2019-11-26 10:04:57 +1100

    Initial commit

---
+++ Assets/Scenes/Scripts/FPSCounter.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    public int frameRange = 60;
+
+    public int AverageFPS { get; private set; }
+    int[] FPSBuffer;
+    int FPSBufferIndex;
+
+    public int HighestFPS { get; private set; }
+    public int LowestFPS { get; private set; }
+
+    void Update()
+    {
+        if (FPSBuffer == null || FPSBuffer.Length != frameRange)
+        {
+            InitializeBuffer();
+        }
+        UpdateBuffer();
+        CalculateFPS();
+    }
+
+    void InitializeBuffer()
+    {
+        if (frameRange <= 0)
+        {
+            frameRange = 1;
+        }
+        FPSBuffer = new int[frameRange];
+        FPSBufferIndex = 0;
+    }
+
+    void UpdateBuffer()
+    {
+        FPSBuffer[FPSBufferIndex++] = (int)(1f / Time.unscaledDeltaTime);
+        if (FPSBufferIndex >= frameRange)
+        {
+            FPSBufferIndex = 0;
+        }
+    }
+
+    void CalculateFPS()
+    {
+        int sum = 0;
+        int highest = 0;
+        int lowest = int.MaxValue;
+        for (int i = 0; i < frameRange; i++)
+        {
+            int fps = FPSBuffer[i];
+            sum += fps;
+            if (fps > highest)
+            {
+                highest = fps;
+            }
+            if (fps < lowest)
+            {
+                lowest = fps;
+            }
+        }
+        AverageFPS = sum / frameRange;
+        HighestFPS = highest;
+        LowestFPS = lowest;
+    }
+}
